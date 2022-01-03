bc7bd84fef065e197f6d8ba9bf905b947ddafda0
Joshua Eldridge <joshuae74@hotmail.com>
2022-01-03 16:41:13 -0600

    Updates to correct stepper direction (Y) and enable the control board fan while steppers are active.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -145,7 +145,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "GL1.1"
+#define CUSTOM_MACHINE_NAME "GL1.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1333,7 +1333,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR true
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
